0813680159f4bebd5c90bba21f3777aea10bd9f6
Jennifer Rosa <rosajen27@gmail.com>
2020-07-11 20:11:11 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(3000)
+
+ 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+ 
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+})
