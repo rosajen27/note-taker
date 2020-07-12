94c3fadf0efd7506a893157f17e0fd6598c3ed22
Jennifer Rosa <rosajen27@gmail.com>
2020-07-11 21:02:01 -0400

    require routing

--- server.js
+++ server.js
@@ -5,22 +5,13 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080
  
-app.get('/', function (req, res) {
-  res.send('Hello World');
-})
-
- 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse application/json
 app.use(bodyParser.json())
- 
-app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
-})
+
+require('./routing/html-routes.js')(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
