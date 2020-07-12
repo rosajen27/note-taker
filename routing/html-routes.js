15aa66d58053865402484ccca3e6af59f22ad5d0
Jennifer Rosa <rosajen27@gmail.com>
2020-07-11 20:41:31 -0400

    Update html-routes.js

--- routing/html-routes.js
+++ routing/html-routes.js
@@ -1,5 +1,12 @@
 const path = require('path');
+const { reduce } = require('../db/notes-data');
 
 module.exports = function (app) {
+    app.get('/notes', function(req, res) {
+        res.sendFile(path.join(__dirname + '/../public/notes.html'));
+    });
     
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname + /../public/indexedDB.html));
+    });
 }
