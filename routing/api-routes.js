51eaccedae11a15b483f05c7d9657a055c695097
Jennifer Rosa <rosajen27@gmail.com>
2020-07-12 19:20:42 -0400

    runnotesquery and post api route

--- routing/api-routes.js
+++ routing/api-routes.js
@@ -3,5 +3,12 @@ const notesData = require ('../db/notes-data.js');
 module.exports = function (app) {
     app.get('/api/notes', function (req, res) {
         res.json(notesData);
+    });;
+
+    app.post('/api/notes', function (req, res) {
+        if(notesData.length < 0) {
+            notesData.push(req.body);
+            res.json(true);
+        }
     });
 }
