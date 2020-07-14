643da9c3f800a38d11bd9a2bb0d2430c179b4bbe
Jennifer Rosa <rosajen27@gmail.com>
2020-07-14 11:30:09 -0400

    delete json & add lorem ipsum on first sample note

--- db/notes-data.js
+++ db/notes-data.js
@@ -1,7 +1,7 @@
 const notesArray = [
     {
-        title:"Test Title",
-        text:"Test text"
+        title:"My First Note",
+        text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer enim justo, egestas at rhoncus in, euismod a risus. Praesent id magna ac libero tincidunt eleifend at vitae massa. Pellentesque feugiat leo a elit vehicula, eget viverra diam pharetra. Vivamus condimentum rhoncus orci vitae hendrerit. Nam at neque sit amet odio pharetra venenatis ut eu turpis. Nunc euismod feugiat porttitor. Sed a nisl vitae purus efficitur auctor ac eget quam. Vestibulum vitae dolor et mauris egestas commodo id eu diam."
     }
 ];
 
