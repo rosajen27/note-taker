56c18731cbddc0d633bf3a33d80ed818bbae1744
Jennifer Rosa <rosajen27@gmail.com>
2020-07-14 21:15:39 -0400

    post new note and give each note a unique ID

--- db/notes-data.js
+++ db/notes-data.js
@@ -1,8 +1,3 @@
-const notesArray = [
-    {
-        title:"My First Note",
-        text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer enim justo, egestas at rhoncus in, euismod a risus. Praesent id magna ac libero tincidunt eleifend at vitae massa. Pellentesque feugiat leo a elit vehicula, eget viverra diam pharetra. Vivamus condimentum rhoncus orci vitae hendrerit. Nam at neque sit amet odio pharetra venenatis ut eu turpis. Nunc euismod feugiat porttitor. Sed a nisl vitae purus efficitur auctor ac eget quam. Vestibulum vitae dolor et mauris egestas commodo id eu diam."
-    }
-];
+const notesArray = [];
 
 module.exports = notesArray;
--- routing/api-routes.js
+++ routing/api-routes.js
@@ -1,13 +1,21 @@
-var notesData = require('../db/notes-data.js');
+const notesData = require ('../db/notes-data.js');
 
 module.exports = function (app) {
+
+    // get notes data
     app.get('/api/notes', function (req, res) {
         res.json(notesData);
     });
 
+    // post new note and give each note a unique ID
     app.post('/api/notes', function (req, res) {
+        const { body } = req;
+
+        body.id = notesData.length + 1;
+
         notesData.push(req.body);
         res.json(true);
-    });
 
+        console.log(notesData);
+    });
 }
